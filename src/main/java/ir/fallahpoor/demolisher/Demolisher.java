5ef58e98f79fbaeaf4433a6a0d733dc886871930
Masood Fallahpoor <m.fallahpour@parham-co.ir>
2017-02-20 12:25:28 +0330

    Add copyright notice to all source code files

--- src/main/java/ir/fallahpoor/demolisher/Demolisher.java
+++ src/main/java/ir/fallahpoor/demolisher/Demolisher.java
@@ -1,3 +1,20 @@
+/*
+ *
+ *     Copyright (C) 2017 Masood Fallahpoor
+ *     This file is part of demolisher.
+ *     demolisher is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *     demolisher is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *     GNU General Public License for more details.
+ *     You should have received a copy of the GNU General Public License
+ *     along with demolisher. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package ir.fallahpoor.demolisher;
 
 import ir.fallahpoor.demolisher.FileUtils.DeleteResult;
